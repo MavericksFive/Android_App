c992bea3f5be5e8b7ccfd9b2a128151a38340cde
MavericksFive <95299145+MavericksFive@users.noreply.github.com>
2023-03-08 15:00:35 +0100

    Added pokedex database

--- app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
+++ app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import fr.imt.pokedex.adapter.PokemonListAdapter
+import fr.imt.pokedex.data.DBHelper
 import fr.imt.pokedex.data.Datasource
 import fr.imt.pokedex.model.Pokemon
 import java.io.IOException
@@ -23,7 +24,8 @@ class PokedexListActivity: AppCompatActivity() {
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
 
-        val dataset: List<Pokemon> = Datasource().loadPokemons()
+        //val dataset: List<Pokemon> = Datasource().loadPokemons()
+        val dataset: List<Pokemon> = DBHelper(this).getAllPokemons()
 
 
         val editText = findViewById<EditText>(R.id.pokedex_list_view_filter_text)
